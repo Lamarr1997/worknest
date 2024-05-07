29366a84331714f83232a3c538dc7380748ac90b
Lamarr Paul <Lamarr_paul97@outlook.com>
2024-05-07 12:47:47 +0100

    completed showing tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :task
+  has_many :tasks
+  has_many :bookings
+  
   # do they have many booking through the task
   # also do they have many booking as well.
 end
