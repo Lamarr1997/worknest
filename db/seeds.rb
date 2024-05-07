29464631a04d87130e7617c3935a54f394f1874e
dev <dev.h@hotmail.co.uk>
2024-05-07 12:39:12 +0100

    intial seedfile test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Task.delete_all
+User.delete_all
+
+gloria = User.create(username: "GloriaWoo", first_name: "Gloria", last_name: "Gilbert", email: "g1@test.com", password: "123456")
+lamarr = User.create(username: "LamarrWoo", first_name: "Lamarr", last_name: "Paul", email: "l2@test.com", password: "123456")
+dev = User.create(username: "DevWoo", first_name: "Dev", last_name: "isTheBest", email: "d3@test.com", password: "123456")
+
+users = [gloria, lamarr, dev]
+
+10.times do
+  task = Task.new(
+    title: Faker::Hobby.activity,
+    location: "London",
+    description: Faker::Lorem.sentence,
+    price: rand(10...100),
+    user: users.sample
+  )
+  task.save
+end
