1fae9b7eb484bda563f5b9122f81d5046b6f3ad7
dev <dev.h@hotmail.co.uk>
2024-05-08 13:27:04 +0100

    dashboard controller basic function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'dashboards/mytasks', as: "mytasks"
+  get 'dashboards/myjobs', as: "myjobs"
+  get 'dashboards/myprofile', as: "myprofile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get '/about', to: 'pages#about'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
